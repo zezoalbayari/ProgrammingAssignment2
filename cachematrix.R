91e512ef39df423e5b4e5f030a899f8e3eaed2fe
zezoalbayari <zezo.albayari@gmail.com>
2020-10-11 15:25:54 +0300

    Added descriptive comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a file for a function which invert a matrix and stores the result in cache
+## instead of doing it from scratch on each call
+## The file contains two functions, one which holds the matrix object and the invertible matrix if calculated before,
+## and the second one, calculates the invertible matrix
 
-## Write a short comment describing this function
+## this function initiate the matrix object and stores its invertible matrix in list
+
 
 makeCacheMatrix <- function(x = matrix()) {
   invert <- NULL
@@ -18,7 +21,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function checks if there is a calculated invertible matrix and return it, and if not it does calculate it 
 
 cacheSolve <- function(x, ...) {
   invert <- x$getinvert()
@@ -27,6 +30,7 @@ cacheSolve <- function(x, ...) {
     return(invert)
   }
   data <- x$get()
+## Here we're testing the matrix if it's invertible to prevent R from throwing an ERROR.
   test <- try(solve(m),silent=T)
   if(is.na(as.logical(test))){
     message("The matrix is not invertible")
